4d175266251b81318422fe0ae2ce6b484f3b4711
sigorjav <sigorjavhan@gmail.com>
2023-06-11 22:42:10 +0900

    프로그램 초기 구축

---
+++ src/main/java/com/sigorjav/sample_ordersystem/SampleOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sigorjav.sample_ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sigorjav/sample_ordersystem/SampleOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigorjav.sample_ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
